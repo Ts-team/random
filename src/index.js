6551a6399dfefb04c9f6b02cf76ad7eec2799f89
Guillaume Martinez <martinezguillaum@gmail.com>
2017-01-27 16:26:12 +0100

    Add initial project + eslint configuration

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import {
+  createElement as $,
+} from 'react';
+import ReactDOM from 'react-dom';
+import cloth from './threed/cloth';
+
+ReactDOM.render(
+  $(cloth),
+  document.getElementById('root'),
+);
