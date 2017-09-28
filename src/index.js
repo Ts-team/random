1f50f276974bd36049e6a0f6e96c0c68fdee2209
Guizmo Martinez <martinezguillaume@users.noreply.github.com>
2017-09-28 10:46:00 +0200

    Update & optimize scene

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import {
   createElement as $,
 } from 'react';
 import ReactDOM from 'react-dom';
-import cloth from './threed/cloth';
+import cloth from './threed/sculpture';
 
 ReactDOM.render(
   $(cloth),
